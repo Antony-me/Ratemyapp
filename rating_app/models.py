0b3c646d9e94811e4166f280bb043d4941312036
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-27 14:16:44 +0300

    Added profile and project modals

--- rating_app/models.py
+++ rating_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.jpg', upload_to='avatars/')
+    bio = models.TextField(max_length=500, blank=True, default=f'Hello, I am new here!')
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='images/')
+    live_link = models.URLField()
+    description = models.TextField(blank=True)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
