4d7c634f91a91390aa9350c4a41cd87595ef8ec2
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-27 14:13:19 +0300

    Added the deafault url, templates and app urls

--- main_app/settings.py
+++ main_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rating_app.apps.RatingAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- rating_app/views.py
+++ rating_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    home view function
+    """
+    return render(request, 'all_projects/index.html')
