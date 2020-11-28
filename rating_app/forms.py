284cd9bc6d6b84a2646c372df8cb82876af15c84
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-28 21:05:09 +0300

    Added update user and profile forms

--- rating_app/forms.py
+++ rating_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post, Profile
+from django.contrib.auth.models import User
 
 class ProfileModelForm(forms.ModelForm):
     class Meta:
@@ -15,3 +16,15 @@ class NewPostForm(forms.ModelForm):
             'tags': forms.CheckboxSelectMultiple(),
         }        
 
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'name','avatar', 'bio']
--- rating_app/views.py
+++ rating_app/views.py
@@ -2,7 +2,7 @@ from rating_app.models import Post, Rating
 from django.shortcuts import render, redirect
 from .models import Profile
 from django.contrib.auth.models import User
-from .forms import NewPostForm
+from .forms import NewPostForm, UpdateUserForm, UpdateUserProfileForm
 # Create your views here.
 def index(request):
     """
@@ -26,3 +26,26 @@ def new_post(request):
     else:
         form = NewPostForm()
     return render(request, 'newpost.html', {"form": form})
+
+def edit_profile(request):
+    
+    
+    if request.method == "POST":
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile')
+            # return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+
+    params = {
+        # 'images' : images,   
+        'user_form': user_form,
+        'prof_form': prof_form,
+        
+    }
+    return render(request, 'new_profile.html', params )
