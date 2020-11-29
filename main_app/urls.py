21b2415c2fd4bbbbb5424f5d25d20ede109b4be0
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-30 02:16:34 +0300

    Deployment to heroku

--- main_app/urls.py
+++ main_app/urls.py
@@ -30,6 +30,3 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
   
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
